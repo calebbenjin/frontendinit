bebc95765579562fc40ca988e18b814e8e448a8e
calebbenjin <calebben94@gmail.com>
2023-02-03 01:18:43 +0100

    TypeForm Integration

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -63,10 +63,10 @@ export default function HeroSection() {
                 </p>
                 <div className="mt-8 flex gap-x-4 sm:justify-center">
                   <Link
-                    href="#"
+                    href="https://xv9ksjbk76s.typeform.com/to/QFnRUXAO"
                     className="inline-block rounded-lg bg-indigo-600 px-4 py-3 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
                   >
-                    Join the community is free
+                    Join the community
                     <span className="text-indigo-200" aria-hidden="true">
                       &rarr;
                     </span>
